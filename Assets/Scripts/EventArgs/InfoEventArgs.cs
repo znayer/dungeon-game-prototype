d07fed9ac3d91f1936dd1466784a03293b8257cd
Zain Nayer <znayer0@gmail.com>
2018-04-12 14:56:32 -0700

    Input manager

---
+++ Assets/Scripts/EventArgs/InfoEventArgs.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class InfoEventArgs<T> : EventArgs {
+
+	public T info;
+
+	public InfoEventArgs(){
+		info = default(T);
+	}
+
+	public InfoEventArgs(T info){
+		this.info = info;
+	}
+}
