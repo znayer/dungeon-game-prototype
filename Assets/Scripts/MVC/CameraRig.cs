9bf24844684ffe9a5a81564577f365bd4e44f3eb
Zain Nayer <znayer0@gmail.com>
2018-04-13 01:56:00 -0700

    More view model components

---
+++ Assets/Scripts/MVC/CameraRig.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraRig : MonoBehaviour {
+
+	public float speed = 3f;
+	public Transform follow;
+	Transform _transform;
+
+	// Use this for initialization
+	void Awake () {
+		_transform = transform;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (follow){
+			_transform.position = Vector3.Lerp(_transform.position, follow.position, speed * Time.deltaTime);
+		}
+		
+	}
+}
