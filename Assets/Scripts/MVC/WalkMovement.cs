db1e2964a5faedb682fb608ab4c699a16d4e8797
Zain Nayer <znayer0@gmail.com>
2018-05-07 14:16:31 -0700

    Walk movement

--- Assets/Scripts/MVC/WalkMovement.cs
+++ Assets/Scripts/MVC/WalkMovement.cs
@@ -22,7 +22,65 @@ public class WalkMovement : Movement {
 		return base.ExpandSearch(from, to);
 	}
 
-	public overried IEnumerator Traverse (Tile tile){
-		
+	public override IEnumerator Traverse (Tile tile){
+		unit.Place(tile);
+
+		// Build list of waypoints
+		List<Tile> targets = new List<Tile>();
+
+		while(tile != null){
+			targets.Insert(0, tile);
+			tile = tile.prev;
+		}
+
+		// Move  to each waypoint
+		for (int i = 1; i < targets.Count; i++){
+			Tile from = targets[i-1];
+			Tile to = targets[i];
+
+			Directions dir = from.GetDirection(to);
+
+			if(unit.dir != dir){
+				yield return StartCoroutine(Turn(dir));
+			}
+
+			if (from.height == to.height){
+				yield return StartCoroutine(Walk(to));
+			}
+			else{
+				yield return StartCoroutine(Jump(to));
+			}
+		}
+
+		yield return null;
+	}
+
+	// Change this later probably
+	IEnumerator Walk(Tile target){
+		Tweener tweener = transform.MoveTo(target.center, 0.5f, EasingEquations.Linear);
+
+		Tweener t2 = jumper.MoveToLocal(new Vector3(0, Tile.stepHeight * 2f, 0), 
+			tweener.easingControl.duration / 2f, 
+			EasingEquations.EaseOutQuad);
+
+		while (tweener != null){
+			yield return null;
+		}
+	}
+
+	// Change this later probably
+	IEnumerator Jump (Tile to){
+		Tweener tweener = transform.MoveTo(to.center, 0.5f, EasingEquations.Linear);
+
+  		Tweener t2 = jumper.MoveToLocal(new Vector3(0, Tile.stepHeight * 2f, 0), 
+  			tweener.easingControl.duration / 2f, 
+  			EasingEquations.EaseOutQuad);
+
+  		t2.easingControl.loopCount = 1;
+  		t2.easingControl.loopType = EasingControl.LoopType.PingPong;
+
+  		while (tweener != null){
+    		yield return null;
+  		}
 	}
 }
