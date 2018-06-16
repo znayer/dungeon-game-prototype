dd7eb40256aa0ff5d06a34de0fc8e68246427ab1
Zain Nayer <znayer0@gmail.com>
2018-06-16 11:02:59 -0700

    Teleport Movement Fixed

--- Assets/Scripts/MVC/TeleportMovement.cs
+++ Assets/Scripts/MVC/TeleportMovement.cs
@@ -1,16 +1,24 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
+using System.Collections;
 
-public class TeleportMovement : MonoBehaviour {
+public class TeleportMovement : Movement 
+{
+  public override IEnumerator Traverse (Tile tile)
+  {  
+    unit.Place(tile);
+    Tweener spin = jumper.RotateToLocal(new Vector3(0, 360, 0), 0.5f, EasingEquations.EaseInOutQuad);
+    spin.easingControl.loopCount = 1;
+    spin.easingControl.loopType = EasingControl.LoopType.PingPong;
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    Tweener shrink = transform.ScaleTo(Vector3.zero, 0.5f, EasingEquations.EaseInBack);
+    while (shrink != null){
+		yield return null;
+    }
+    transform.position = tile.center;
+
+    Tweener grow = transform.ScaleTo(Vector3.one, 0.5f, EasingEquations.EaseOutBack);
+    while (grow != null){
+      yield return null;
+    }
+  }
 }
